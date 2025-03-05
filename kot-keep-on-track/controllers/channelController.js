dbb7598c3ee10aa6961a13a5b61338e8f7a711f4
GChamuscaWebDeveloper <jessicavtc@gmail.com>
2025-03-05 02:57:42 -0300

    Update channelController.js

--- kot-keep-on-track/controllers/channelController.js
+++ kot-keep-on-track/controllers/channelController.js
@@ -1,6 +1,18 @@
-// Controlador para gerenciar canais e métricas (opcional)
+// controllers/channelController.js
 async function getChannelDashboard(req, res) {
-  res.send('Dashboard do canal');
+  // Simulação de dados do dashboard do canal.
+  // Em uma implementação real, você buscará os dados do banco e das APIs integradas.
+  const dashboardData = {
+    channelId: req.query.channelId || 1,
+    views: 12345,
+    subscribers: 678,
+    bestVideo: {
+      title: "Vídeo Exemplo",
+      views: 2345,
+    },
+    revenue: 1500.00,
+  };
+  res.json(dashboardData);
 }
 
 module.exports = {
