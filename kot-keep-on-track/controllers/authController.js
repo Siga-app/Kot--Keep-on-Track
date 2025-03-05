b8e7e767ccf840ffaaef65573aa7a917345cf6db
GChamuscaWebDeveloper <jessicavtc@gmail.com>
2025-03-05 19:42:49 -0300

    Update authController.js

--- kot-keep-on-track/controllers/authController.js
+++ kot-keep-on-track/controllers/authController.js
@@ -1,5 +1,5 @@
 // controllers/authController.js
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const { createUser, findUserByEmail } = require('../models/User');
 
