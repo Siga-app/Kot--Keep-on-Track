91da56c9014e3e8a37f635a72c2f04f837db4c30
GChamuscaWebDeveloper <jessicavtc@gmail.com>
2025-03-05 02:56:59 -0300

    Update financialRoutes.js

--- kot-keep-on-track/routes/financialRoutes.js
+++ kot-keep-on-track/routes/financialRoutes.js
@@ -1,9 +1,13 @@
+// routes/financialRoutes.js
 const express = require('express');
 const router = express.Router();
 const { createFinancialRecord, getFinancialRecords } = require('../controllers/financialController');
 const auth = require('../middleware/auth');
 
+// Endpoint para criar um registro financeiro
 router.post('/', auth, createFinancialRecord);
+
+// Endpoint para listar registros financeiros
 router.get('/', auth, getFinancialRecords);
 
 module.exports = router;
