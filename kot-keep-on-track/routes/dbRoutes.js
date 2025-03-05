e60bf178a189d287fc90f22bfd47607c0247bd95
GChamuscaWebDeveloper <jessicavtc@gmail.com>
2025-03-05 19:44:17 -0300

    Update dbRoutes.js

--- kot-keep-on-track/routes/dbRoutes.js
+++ kot-keep-on-track/routes/dbRoutes.js
@@ -1,9 +1,9 @@
-// dbRoutes.js
+// routes/dbRoutes.js
 const express = require('express');
 const router = express.Router();
 const { migrate } = require('../controllers/migrateController');
 
-// Rota para criar a tabela "users" (sem usar terminal)
+// Rota para criar a tabela "users" sem usar o terminal
 router.get('/migrate', migrate);
 
 module.exports = router;
