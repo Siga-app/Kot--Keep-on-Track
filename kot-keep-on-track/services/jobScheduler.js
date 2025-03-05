f2b9e829f0410c7c2f9eb1062767d10cce111ae3
GChamuscaWebDeveloper <jessicavtc@gmail.com>
2025-03-05 02:58:55 -0300

    Update jobScheduler.js

--- kot-keep-on-track/services/jobScheduler.js
+++ kot-keep-on-track/services/jobScheduler.js
@@ -1,7 +1,9 @@
+// services/jobScheduler.js
 const cron = require('node-cron');
 const integrationService = require('./integrationService');
 
+// Agendar job para atualizar os dados das APIs externas a cada 24 horas
 cron.schedule('0 0 * * *', async () => {
-  console.log('Atualizando dados das APIs externas...');
+  console.log('Executando job agendado: Atualizando dados das APIs externas...');
   await integrationService.atualizarDados();
 });
