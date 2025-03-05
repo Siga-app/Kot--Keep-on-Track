c39326b76873fc7bbf7cf90f607bbdfad986dd64
GChamuscaWebDeveloper <jessicavtc@gmail.com>
2025-03-05 02:57:58 -0300

    Update channelRoutes.js

--- kot-keep-on-track/routes/channelRoutes.js
+++ kot-keep-on-track/routes/channelRoutes.js
@@ -1,8 +1,10 @@
+// routes/channelRoutes.js
 const express = require('express');
 const router = express.Router();
 const { getChannelDashboard } = require('../controllers/channelController');
 const auth = require('../middleware/auth');
 
+// Endpoint para obter o dashboard do canal
 router.get('/dashboard', auth, getChannelDashboard);
 
 module.exports = router;
