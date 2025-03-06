028b5a6c0cc637337c281e9ebb3cd002896695ed
GChamuscaWebDeveloper <jessicavtc@gmail.com>
2025-03-05 21:07:36 -0300

    Update Navbar.js

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,5 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
 function Navbar() {
-  return <nav>Navbar</nav>;
+  return (
+    <nav style={{ padding: '10px', backgroundColor: '#333', color: '#fff' }}>
+      <Link to="/" style={{ color: '#fff', marginRight: '10px' }}>Dashboard</Link>
+      <Link to="/login" style={{ color: '#fff', marginRight: '10px' }}>Login</Link>
+      <Link to="/register" style={{ color: '#fff' }}>Registro</Link>
+    </nav>
+  );
 }
+
 export default Navbar;
