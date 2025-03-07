a9f5b455eb08f91522db9044018767e642611936
GChamuscaWebDeveloper <jessicavtc@gmail.com>
2025-03-07 10:20:43 -0300

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
-// client/src/App.js
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Landing from './routes/Landing';
 import Dashboard from './routes/Dashboard';
+import GestaoFinanceira from './routes/GestaoFinanceira';
+import MetricasCanais from './routes/MetricasCanais';
+import Configuracoes from './routes/Configuracoes';
 import Login from './routes/Login';
 import Register from './routes/Register';
 
@@ -14,6 +16,9 @@ function App() {
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/gestao-financeira" element={<GestaoFinanceira />} />
+        <Route path="/metricas-canais" element={<MetricasCanais />} />
+        <Route path="/configuracoes" element={<Configuracoes />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
       </Routes>
